521d4d48d37e4221bf11fc63b4923efc6a39c933
Samuel Kimani <samkeyms@gmail.com>
2021-09-22 12:30:52 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,17 +13,17 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="icon" href="images/favicon.png" />
         <meta
           property="og:title"
-          content="Hotflix - watch TV shows online, watch movies online"
+          content="HBTV - watch TV shows online, watch movies online"
         />
         <meta
           property="og:image"
-          content="https://hotflix-clone.vercel.app/images/misc/background.jpg"
+          content="https://hbtv.vercel.app/images/misc/background.jpg"
         />
         <meta
           property="og:description"
-          content="Hotflix is a Netflix clone that comes with all the basic functionalities Netflix offers."
+          content="Homeboyz TV is a premium subscription and video on demand platform showcasing original pre recorded content as well as live streamed music and sports events."
         />
-        <meta property="og:url" content="https://hotflix-clone.vercel.app/" />
+        <meta property="og:url" content="https://hbtv.vercel.app/" />
       </Head>
       <Provider session={pageProps.session}>
         <Layout>
